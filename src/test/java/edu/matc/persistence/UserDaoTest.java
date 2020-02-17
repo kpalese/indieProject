6b89ea75e2a94664aaaba6e0b7e8380268863881
kpalese <kelly.palese@gmail.com>
2020-02-16 21:47:44 -0600

    Working on configs for test database

---
+++ src/test/java/edu/matc/persistence/UserDaoTest.java
@@ -0,0 +1,14 @@
+package edu.matc.persistence;
+
+import edu.matc.entity.User;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserDaoTest {
+
+}
