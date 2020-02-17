fae086a2d5e5f6b269cb652049513dd8d8b732d7
kpalese <kelly.palese@gmail.com>
2020-02-16 20:57:23 -0600

    Added User java bean

---
+++ src/main/java/entity/User.java
@@ -0,0 +1,98 @@
+package entity;
+
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+/**
+ * A java bean to represent a user
+ * @author Kelly Palese
+ */
+@Entity(name = "User")
+@Table(name = "Users")
+public class User {
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO, generator="native")
+    @GenericGenerator(name = "native",strategy = "native")
+    private int userId;
+
+    @Column(name = "user_name")
+    private String userName;
+
+    @Column(name = "user_password")
+    private String userPassword;
+
+    /**
+     * Instantiates a new User (empty constructor)
+     */
+    public User() {
+    }
+
+    /**
+     * Instantiates a new User.
+     *
+     * @param userId   the user's id
+     * @param userName    the user name
+     * @param userPassword    the user's password
+     */
+    public User(int userId, String userName, String userPassword) {
+        this.userId = userId;
+        this.userName = userName;
+        this.userPassword = userPassword;
+    }
+
+    /**
+     * Gets user id.
+     *
+     * @return the user id
+     */
+    public int getUserId() {
+        return userId;
+    }
+
+    /**
+     * Sets user id.
+     *
+     * @param userId the user id
+     */
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    /**
+     * Gets user name.
+     *
+     * @return the user name
+     */
+    public String getUserName() {
+        return userName;
+    }
+
+    /**
+     * Sets user name.
+     *
+     * @param userName the user name
+     */
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    /**
+     * Gets user password.
+     *
+     * @return the user password
+     */
+    public String getUserPassword() {
+        return userPassword;
+    }
+
+    /**
+     * Sets user password.
+     *
+     * @param userPassword the user password
+     */
+    public void setUserPassword(String userPassword) {
+        this.userPassword = userPassword;
+    }
+}
