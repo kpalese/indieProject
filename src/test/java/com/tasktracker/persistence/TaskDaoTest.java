f851f76bbf939303c89260af82fe5e34571b44c2
kpalese <kelly.palese@gmail.com>
2020-04-08 16:54:34 -0500

    First round of Dao tests complete. Need to review closer for more specific scenarios to include

--- src/test/java/com/tasktracker/persistence/TaskDaoTest.java
+++ src/test/java/com/tasktracker/persistence/TaskDaoTest.java
@@ -1,6 +1,109 @@
 package com.tasktracker.persistence;
 
-//TODO: Need to create tests
+import com.tasktracker.entity.Task;
+import com.tasktracker.entity.User;
+import com.tasktracker.test.util.Database;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class TaskDaoTest {
+    GenericDao genericDao;
+
+    @BeforeEach
+    void setUp() {
+
+        Database database = Database.getInstance();
+        database.runSQL("cleandb.sql");
+
+        genericDao = new GenericDao(Task.class);
+    }
+
+    /**
+     * Verify successful retrieval of a task
+     */
+    @Test
+    void getByIdSuccess() {
+        Task retrievedTask = (Task)genericDao.getById(2);
+        assertNotNull(retrievedTask);
+        assertEquals("Call Mom", retrievedTask.getName());
+        LocalDate expectedDate = LocalDate.parse("2020-05-01");
+        assertEquals(expectedDate, retrievedTask.getDate());
+    }
+
+    /**
+     * Verify successful update of a task
+     */
+    @Test
+    void updateSuccess() {
+        String newTaskName = "Mow lawn";
+        Task taskToUpdate = (Task)genericDao.getById(1);
+        taskToUpdate.setName(newTaskName);
+        genericDao.saveOrUpdate(taskToUpdate);
+        Task retrievedTask = (Task)genericDao.getById(1);
+        assertEquals(taskToUpdate, retrievedTask);
+    }
+
+    /**
+     * Verify successful insert of a task
+     */
+    @Test
+    void insertSuccess() {
+        String name = "Testing a new task";
+        LocalDate date = LocalDate.parse("2020-05-23");
+        String frequency = "once";
+        String notes = "This new task is going to be great!";
+        boolean complete = false;
+        GenericDao userDao = new GenericDao(User.class);
+        User user = (User)userDao.getById(1);
+
+        Task newTask = new Task(name, date, frequency, notes, complete, user);
+        int id = genericDao.insert(newTask);
+        assertNotEquals(0,id);
+        Task insertedTask = (Task)genericDao.getById(id);
+        assertEquals(newTask, insertedTask);
+    }
+
+    /**
+     * Verify successful delete of a task
+     */
+    @Test
+    void deleteSuccess() {
+        genericDao.delete(genericDao.getById(3));
+        assertNull(genericDao.getById(3));
+    }
+
+    /**
+     * Verify successful retrieval of all tasks
+     */
+    @Test
+    void getAllSuccess() {
+        List<Task> tasks = genericDao.getAll();
+        assertEquals(3, tasks.size());
+    }
+
+    /**
+     * Verify successful get by property (equal match)
+     */
+    @Test
+    void getByPropertyEqualSuccess() {
+        List<Task> events = genericDao.getByPropertyEqual("user", 1);
+        assertEquals(2, events.size());
+        assertEquals(1, events.get(0).getId());
+    }
+
+    /**
+     * Verify successful get by property (like match)
+     */
+    @Test
+    void getByPropertyLikeSuccess() {
+        List<Task> tasks = genericDao.getByPropertyLike("name", "er");
+        assertEquals(2, tasks.size());
+        assertEquals(1, tasks.get(0).getId());
+        assertEquals(3, tasks.get(1).getId());
+    }
 }
--- src/test/resources/cleandb.sql
+++ src/test/resources/cleandb.sql
@@ -17,3 +17,9 @@ INSERT INTO `EVENT`
 VALUES (1,'Meeting with Ann','2020-02-29','09:30:00','11:00:00','This is a very important meeting. Don\'t forget to bring a pen.',1),
        (2,'Dentist','2020-03-03','12:30:00',NULL,'Not sure how long this will last.',1),(3,'Fundraiser','2020-03-03','16:00:00','20:00:00',NULL,1),
        (4,'Doctor appt for J','2020-04-07','07:45:00',NULL,'Checkup',3),(5,'Phone interview','2020-04-06','10:00:00','10:30:00','They will call me',2);
+
+delete from TASK;
+INSERT INTO TASK
+VALUES (1, 'Replace furnace filter', '2020-05-26', 'once', 'Use the ABC321 type', 0, 1),
+       (2, 'Call Mom', '2020-05-01', 'weekly',NULL, 0, 1),
+       (3, 'Water the plants', '2020-04-10', 'weekly',NULL, 0, 2);
