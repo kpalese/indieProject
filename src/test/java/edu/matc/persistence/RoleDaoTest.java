4085334265ae69ae08a0a0c16c8e21cde5177bd5
kpalese <kelly.palese@gmail.com>
2020-04-08 16:03:31 -0500

    Updated GenericDao equals and like methods to accept Object, completed RoleDaoTest

--- src/test/java/edu/matc/persistence/RoleDaoTest.java
+++ src/test/java/edu/matc/persistence/RoleDaoTest.java
@@ -1,5 +1,96 @@
 package edu.matc.persistence;
 
-//TODO: Need to create tests
+import edu.matc.entity.Role;
+import edu.matc.entity.User;
+import edu.matc.test.util.Database;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class RoleDaoTest {
+    GenericDao genericDao;
+
+    @BeforeEach
+    void setUp() {
+
+        Database database = Database.getInstance();
+        database.runSQL("cleandb.sql");
+
+        genericDao = new GenericDao(Role.class);
+    }
+
+    /**
+     * Verify successful retrieval of a role
+     */
+    @Test
+    void getByIdSuccess() {
+        GenericDao userDao = new GenericDao(User.class);
+        User user = (User)userDao.getById(2);
+        Role role = (Role)genericDao.getById(2);
+        assertNotNull(role);
+        assertEquals(user, role.getUser());
+    }
+
+    /**
+     * Verify successful insert of a role
+     */
+    @Test
+    void insertSuccess() {
+        GenericDao userDao = new GenericDao(User.class);
+        User user = (User)userDao.getById(4);
+        Role newRole = new Role(user, "user", "FlowerDaisy");
+        user.addRole(newRole);
+
+        int id = genericDao.insert(newRole);
+        Role insertedRole = (Role)genericDao.getById(id);
+        assertNotEquals(0,id);
+        assertEquals(user, insertedRole.getUser());
+    }
+
+    /**
+     * Verify successful delete of a user's role
+     */
+    @Test
+    void deleteSuccess() {
+        genericDao.delete(genericDao.getById(1));
+
+        GenericDao userDao = new GenericDao(User.class);
+        User userWithDeletedRole = (User)userDao.getById(1);
+
+        assertNull(genericDao.getById(1));
+        assertEquals(0, userWithDeletedRole.getRoles().size());
+    }
+
+    /**
+     * Verify successful retrieval of all roles
+     */
+    @Test
+    void getAllSuccess() {
+        List<Role> roles = genericDao.getAll();
+        assertEquals(4, roles.size());
+    }
+
+    /**
+     * Verify successful get by property (equal match)
+     */
+    @Test
+    void getByPropertyEqualSuccess() {
+        List<Role> roles = genericDao.getByPropertyEqual("roleName", "user");
+        assertEquals(4, roles.size());
+        assertEquals(1, roles.get(0).getId());
+    }
+
+    /**
+     * Verify successful get by property (like match)
+     */
+    @Test
+    void getByPropertyLikeSuccess() {
+        List<Role> roles = genericDao.getByPropertyLike("roleName", "u");
+        assertEquals(4, roles.size());
+        assertEquals(1, roles.get(0).getId());
+    }
 }
--- src/test/resources/cleandb.sql
+++ src/test/resources/cleandb.sql
@@ -1,5 +1,17 @@
 delete from USERS;
-INSERT INTO USERS VALUES (1, 'testUser1', 'mypassword123', true),(2, 'Jane_Doe', 'welcome123', false), (3, 'WaltD', '321xyz', false),(4, 'FlowerDaisy', 'ILikeFlowers', true);
+INSERT INTO USERS
+VALUES (1, 'testUser1', 'mypassword123', true),
+       (2, 'Jane_Doe', 'welcome123', false),
+       (3, 'WaltD', '321xyz', false),
+       (4, 'FlowerDaisy', 'ILikeFlowers', true);
+
+delete from USER_ROLES;
+INSERT INTO USER_ROLES
+VALUES (1, 'user', 'testUser1', 1),
+       (2, 'user', 'Jane_Doe', 2),
+       (3, 'user', 'WaltD', 3),
+       (4, 'user', 'FlowerDaisy', 4);
 
 delete from EVENT;
-INSERT INTO `EVENT` VALUES (1,'Meeting with Ann','2020-02-29','09:30:00','11:00:00','This is a very important meeting. Don\'t forget to bring a pen.',1),(2,'Dentist','2020-03-03','12:30:00',NULL,'Not sure how long this will last.',1),(3,'Fundraiser','2020-03-03','16:00:00','20:00:00',NULL,1),(4,'Doctor appt for J','2020-04-07','07:45:00',NULL,'Checkup',3),(5,'Phone interview','2020-04-06','10:00:00','10:30:00','They will call me',2);
+INSERT INTO `EVENT`
+VALUES (1,'Meeting with Ann','2020-02-29','09:30:00','11:00:00','This is a very important meeting. Don\'t forget to bring a pen.',1),(2,'Dentist','2020-03-03','12:30:00',NULL,'Not sure how long this will last.',1),(3,'Fundraiser','2020-03-03','16:00:00','20:00:00',NULL,1),(4,'Doctor appt for J','2020-04-07','07:45:00',NULL,'Checkup',3),(5,'Phone interview','2020-04-06','10:00:00','10:30:00','They will call me',2);
