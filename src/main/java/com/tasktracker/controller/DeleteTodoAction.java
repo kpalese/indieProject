e696b7db52134fc829e9c4a8cf131bc39817449e
kpalese <kelly.palese@gmail.com>
2020-05-06 16:50:57 -0500

    Added message when user successfully removes to-do item

--- src/main/java/com/tasktracker/controller/DeleteTodoAction.java
+++ src/main/java/com/tasktracker/controller/DeleteTodoAction.java
@@ -7,6 +7,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 @WebServlet(
@@ -15,6 +16,8 @@ import java.io.IOException;
 public class DeleteTodoAction extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        HttpSession session = req.getSession();
+
         //Get to-do to be deleted
         String todoId = req.getParameter("id");
         GenericDao todoDao = new GenericDao(Todo.class);
@@ -23,7 +26,8 @@ public class DeleteTodoAction extends HttpServlet {
         //Delete to-do
         todoDao.delete(todoToDelete);
 
-        //TODO: Message that to-do was successfully deleted?
+        session.setAttribute("userMessage", "The item was successfully deleted!");
+        session.setAttribute("messageClass", "alert-success");
 
         resp.sendRedirect(req.getContextPath() + "/users/viewPlanner");
     }
